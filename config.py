f3835e0aba0e068c5f53479cea2589eb15340b41
Peter Krantz <zyrree@gmail.com>
2020-05-17 09:58:26 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,2 @@
+api_key = 'INSERT_API_KEY'
+api_secret = 'INSERT_API_SECRET'
---
+++ trelloUpdate.py
@@ -0,0 +1,36 @@
+from trello import TrelloClient
+import psycopg2
+import config
+
+trelloClient = TrelloClient(
+    api_key=config.api_key,
+    api_secret=config.api_secret
+)
+
+db = psycopg2.connect(host="localhost", database="trelloData", user="pi", password="herman")
+db.set_session(autocommit=True)
+# clear db of tasks
+cur = db.cursor()
+cur.execute("DELETE FROM api_task;")
+cur.close()
+
+cursor = db.cursor()
+query = "INSERT INTO api_task(title, description, done, \"dueDate\", list) VALUES "
+
+# fetch from trelloAPI
+allBoards = trelloClient.list_boards()
+weekBoard = ''
+for board in allBoards:
+    if board.name == 'Week':
+        weekBoard = board
+
+lists = weekBoard.list_lists()
+for list in lists:
+    for card in list.list_cards():
+        query += "('%s','%s','%s','%s','%s'), " % \
+                 (card.name, card.description, str(card.badges['dueComplete']), str(card.due_date), list.name)
+query = query[:-2]
+query += ";"
+cursor.execute(query)
+cursor.close()
+db.close()
